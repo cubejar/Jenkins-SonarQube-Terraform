1e02db157768139ed9a23148cea705f357a44d85
cubejar <raj.avudaiappan@gmail.com>
2023-12-22 09:49:18 -0800

    Minor updates - the region, keypair - tf files

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  # region = "ap-south-1"     #change region as per you requirement
+  region = "us-west-2"     # us-west-2
+}
+
